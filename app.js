cc1e2136bbc341522f93d6d922755699724b2c23
Dilmuradmastoi <webdeveloper1234567890001@gmail.com>
2025-10-25 12:32:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,202 @@
+
+
+////////////////////////////////////
+//      1
+// const input = document.querySelector("#message")
+// const btn = document.querySelector("#btn")
+
+// function testingbtn()  {
+//     console.log  ( "welcome " +  input.value )
+// }
+//////////////////////////////////////
+//    2
+//    const body = document.querySelector("#container")
+// function changeColor (color){
+//     console.log(color) ;
+//     body.style.backgroundColor = color
+// }
+///////////////////////////////////////////////////
+//         3
+//         const input = document.querySelector("#nameinput")
+// const btn = document.querySelector ("#inputbtn")
+
+// function testing()  {
+//     console.log ("Result IS  " + input.value)
+// }
+////////////////////////////////////////////////
+    //         4          
+    // const body = document.querySelector("#container")
+    
+    // function changeColor (color) {
+    //     console.log (color)
+    //     body.style.backgroundColor = color
+    // }
+        ///////////////////////////////////////////////////////
+    //           5
+    // const input = document.querySelector("#num1")
+    // const btn = document.querySelector("#outputbtn")
+    // const output = document.querySelector("#output")
+
+    // function outputtesting() {
+    //     console.log("output  is    " + input.value)
+    //     output.textContent = "you Entered   " +  input.value
+    // }
+    /////////////////////////////////////////
+    //         6
+        // const btn = document.querySelector("#submit_btn")
+
+        // function submit_btn() {
+        //     console.log("btn clicked")
+        // }
+    //////////////////////////////////////////
+    ///         7
+    // const input = document.querySelector("#input-1 " ) 
+    //     input2 = document.querySelector("#input-2")
+    // const btn = document.querySelector("#adding-btn")
+    // const output = document.querySelector("#adding-output")
+
+    // function addingfunction() {
+    //     console.log ( "input:  " + input.value    +  " " + input2.value )
+    //     output.textContent = "complete Name:  " + input.value + " " + input2.value 
+    // }
+k    ///////////////////////////////////////////
+    //          8
+    //    const  input = document.querySelector("#description")
+    //         result = document.querySelector("#description")
+    //     // heading.innerHTML = "#testingpera"
+    //     // console.log("unsid")
+    //      const btn = document.querySelector("#result")
+    //         result = document.querySelector("#result")
+    //     function clearScreen() {
+    //         console.log ( "Updated SuccessFully   "  + input.value    ) 
+    //         input.value =  ""
+    //     }
+
+
+        ////////////////////////////////////////
+    //        9 
+        // const container = document.querySelector("#box")
+        
+        // function changeColor(color) {
+        //     console.log (color);
+        //     box.style.backgroundColor = color
+        // }
+    //////////////////////////////////////////////////////////////////
+    //        10
+    // const input = document.querySelector("#email")
+    // const btn = document.querySelector("#email-btn")
+    // function email () {
+    //     console.log("email Submitted    " + input.value)
+    // }
+        ////////////////////////////////////////////////////
+    //       11
+     
+    /////////////////////////////////////////////////////////
+    //         12
+    // const input = document.querySelector("#inputname")
+    // const btn = document.querySelector("#inputnamebtn")
+    // const container = document.querySelector(".greeting")
+
+    // function nametesting() {
+    //     console.log("hello greeting:   " + input.value)
+    //     container.textContent = "Greeting Is:      "  + input.value
+    // }
+    // ///////////////////////////////////////////////////////
+    //        15
+        // const input = document.querySelector("#check1")
+        // input2 = document.querySelector("#check2")
+        // const output = document.querySelector("#checkpera")
+        
+        // function check2(){
+        //     console.log ("disagree")
+        //     output.textContent = "disageed" 
+        // }
+        // function check() {
+        //     console.log("agree" )
+        //     output.textContent =  "agree"     
+        // }
+        //////////////////////////////////////////////////////
+        ///        11
+        // const pera = document.querySelector("#fontpera") ;
+
+        // function fontpera() {
+        //     console.log("hello world clicked")
+        //     pera.style.fontSize = '24px'
+        // }
+        //////////////////////////////////////////////////
+        
+        // const pera = document.getElementById("#datetime")
+        // const btn = document.getElementById("timebtn") 
+        // const hours = now.getHours () . toString() . padStart(2,0);
+        // const minutes = now.getMinutes () . toString () . padStart(2,0);
+        // const seconds = now.getSeconds () . toString() . padStart(2,0);
+        // console.log ('$(hours): $(minutes): $(seconds)')
+        // function timetesting() {
+        //     console.log("clicked")
+        //     console.log ('$(hours): $(minutes): $(seconds)')
+        //     btn = addEventListener  => {
+        //         console.log = new Date ()
+        //         dateTimeParagraph.textContent = now.toString ()
+        //     } }
+        //   ////////////////////////////////////////////////////
+        // const now = new Date () ;
+        // const hours = now.getHours(). toString().padStart(2,'0');
+        //  const minutes = now.getMinutes(). toString().padStart(2,'0');
+        //   const seconds = now.getSeconds(). toString().padStart(2,'0');
+        //   console.log('${hours}: ${minutes}: ${seconds}');
+
+        /////////////////////////////////////////////////////////
+        //          13
+    //    const image = document.querySelector("#image")
+
+    //    function  testingpic() {
+    //     console.log ("image changed")
+    //     image.src = "https://i.ytimg.com/vi/l4obqgJzmC4/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAxBHXAFgVHaDmc_ogDGW1M9SiFlQ"
+    //    }
+    //////////////////////////////////////////////////////
+    //          14
+    // Select all elements with the class 'highlight'
+    // const heading = document.querySelectorAll("#heading")
+
+    // function testinghead() {
+    //     console.log ("clicekd hi")
+    //     heading.forEach (element => {
+    //         element.style.color = 'orange'
+    //     } ) }
+    //////////////////////////////////////////////////////////
+    //             19
+//     const input = document.querySelector("#userage")
+// const btn = document.querySelector("#useragebtn")
+// function testingage() {
+//     console.log("age inputed  " + input.value)
+//     if (input.value >= 18) {
+//         console.log("eligible")   
+//     } else (
+//             console.log("not eligible")
+//         )}
+    //////////////////////////////////////////////////////////
+//         18
+//  const  heading = document.querySelector("#headingh1")
+//  const btn = document.querySelector("btn1")
+//     function testing() {
+//         console.log("hidden")
+//         heading.innerHTML =  ' '       
+//     }
+// ////////////////////////////////////////////////////
+//   20 
+// const input = document.querySelector ("#clearinput")
+// const btn = document.querySelector("#clearbtn")
+
+// function testingclear() {
+//     console.log ("clear"  )
+//     input.value = ' '
+// }
+
+
+//////////////////////////////////////////////////////
+        // function testing ()  {
+        //     console.log ("hello" + prompt())
+        //        }
+        //        testing()
+
+    
